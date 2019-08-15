56890cb1ba7d7520650c144c005138f0a55e3128
Pedro Araujo <pedro-foa@hotmail.com>
2019-08-15 20:45:55 -0300

    Exercise contact list

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 let app = new Vue({
     el:'#app',
-    data: {
-        primeiroNome: '',
-        segundoNome: '',
-    },  
-    computed: {
-        nomeCompleto: function() {
-            return `${this.primeiroNome} ${this.segundoNome}`
+    data: { 
+        nome: '',
+        contatos: [],
+        aviso: '',
+        timer: null,
+        nomePronto: false
+    },
+    watch: {
+        nome: function() {
+            if(this.timer != null) {
+                clearTimeout(this.timer);
+                this.aviso = "";
+            }
+
+            if(this.nome != '') {
+                this.aviso = "Digitando...";
+                this.nomePronto = false;
+                this.timer = setInterval(this.ficarPronto, 1000);
+            }
         }
-    },   
+    },
+    computed: {
+        totalContatos: function() {
+            return `Total de Contatos: ${this.contatos.length}`
+        }
+    },
+    methods: {
+        ficarPronto: function() {
+            this.aviso = '';
+            this.nomePronto = true;            
+        },
+        addContato: function() {
+            this.contatos.push(this.nome);
+            this.nome = '';
+            this.nomePronto = false;
+        }
+    },
 });
 
 
